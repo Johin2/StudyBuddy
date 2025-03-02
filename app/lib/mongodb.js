7b5a7921e5abfb2fe7f7131bbed30a489a7d718e
Johin2 <johinjohny144@gmail.com>
2025-03-03 01:04:31 +0530

    added mongodb k8s

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/studybuddy"
+const MONGO_URL = "mongodb://mongodb-service:27017/studybuddy"
 
 export async function connectDB() {
     if (mongoose.connection.readyState >= 1) return;
