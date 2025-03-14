e3da607a73508f15111095d75ad0115ecaf09793
Johin2 <johinjohny144@gmail.com>
2025-03-14 22:18:34 +0530

    caching

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,15 +1,30 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const MONGO_URL = process.env.MONGO_URL; 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export async function connectDB() {
-  if (mongoose.connection.readyState >= 1) return;
-
-  try {
-    await mongoose.connect(MONGO_URL);
-    console.log("✅ Connected to MongoDB");
-  } catch (error) {
-    console.error("❌ MongoDB connection failed:", error);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 5000, // Adjust based on your needs
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
