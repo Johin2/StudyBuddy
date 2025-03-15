c107977e49fdd06483fc9b9ce92345b6d18ad0ba
Johin2 <johinjohny144@gmail.com>
2025-03-16 01:53:18 +0530

    seo friendly

--- app/layout.js
+++ app/layout.js
@@ -2,17 +2,19 @@ import "./globals.css";
 import { AuthProvider } from "./Context/AuthContext";
 
 export const metadata = {
-  title: "StuddyBuddy",
+  title: "StudyBuddy",
   description: "App from students, by students, for students",
+  keywords: ["StudyBuddy", "Student App", "Education", "Learning", "Study"],
+  icons: {
+    icon: "/images/favicon.svg",
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <AuthProvider>
       <html lang="en">
-        <body>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </AuthProvider>
   );
